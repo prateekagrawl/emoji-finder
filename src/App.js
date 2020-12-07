12c15e2d14efdf574f62726b4b1099bbe34eeb04
Prateek Agrawal <60268597+prateekagrawl@users.noreply.github.com>
2020-12-08 00:19:00 +0530

    feat: added functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
+const divStyle = {
+  display: "flex",
+  justifyContent: "center",
+  margin: "0.5rem"
+};
 
-  function incrementCounter() {
-    var xlikeCounter = likeCounter + 1;
-    setLikeCounter(xlikeCounter);
+var emojiDictionary = {
+  "😊": "smiling",
+  "😐": "Neutral face",
+  "😳": "disbelief",
+  "😍": "Heart eyes",
+  "😔": "sad",
+  "😃": "happy face",
+  "❤️": "love",
+  "😉": "winking",
+  "😝": "tongue out",
+  "😋": "goofy"
+};
+
+var emojiArray = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState(" ");
+
+  function emojiChangeHandler(event) {
+    var emojiMeaning = event.target.value;
+    meaning = emojiDictionary[emojiMeaning]; //searching in object
+    if (meaning === undefined)
+      meaning = "Sorry 😔,we don't have it in our database";
+    setMeaning(meaning);
   }
+
+  function emojiClickHandler(emoji) {
+    //check whether emoji is present in db or not
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>inside outt</h1>
-      <button onClick={incrementCounter}>Like </button>
-      {likeCounter}
+      <h1>emoji finder</h1>
+      <input
+        style={{ padding: "0.4rem" }}
+        onChange={emojiChangeHandler}
+      ></input>
+      <div style={{ color: "black", margin: "0.5rem" }}> {meaning} </div>
+
+      {/* adding lists */}
+      <div style={divStyle}> Emojis present in our database 👇</div>
+
+      {emojiArray.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ cursor: "pointer", padding: "0.5rem" }}
+            key={emoji}
+          >
+            {emoji}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
